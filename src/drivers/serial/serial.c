774ff4d4d9bb454bee8f1c1b621032104f811bf5
Ajay MT <ajay.tatachar@gmail.com>
2019-05-06 19:59:50 -0500

    remove unnecessary functions and macros

--- src/drivers/serial/serial.c
+++ src/drivers/serial/serial.c
@@ -11,11 +11,14 @@
 // I/O ports.
 // All I/O ports are calculated relative to the data port.
 const unsigned short SERIAL_COM1_BASE = 0x3F8; // COM1 base port.
-#define SERIAL_DATA_PORT(base)          (base)
-#define SERIAL_FIFO_COMMAND_PORT(base)  (base + 2)
-#define SERIAL_LINE_COMMAND_PORT(base)  (base + 3)
-#define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
-#define SERIAL_LINE_STATUS_PORT(base)   (base + 5)
+static inline unsigned short serial_fifo_command_port(const unsigned short base)
+{ return base + 2; }
+static inline unsigned short serial_line_command_port(const unsigned short base)
+{ return base + 3; }
+static inline unsigned short serial_modem_command_port(const unsigned short base)
+{ return base + 4; }
+static inline unsigned short serial_line_status_port(const unsigned short base)
+{ return base + 5; }
 
 // I/O port commands.
 // Tells the serial port to expect first the highest 8 bits on the data port,
@@ -29,40 +32,16 @@ void serial_configure_baud_rate(
   const unsigned short com, const unsigned short divisor
   )
 {
-  outb(SERIAL_LINE_COMMAND_PORT(com), SERIAL_LINE_ENABLE_DLAB);
-  outb(SERIAL_DATA_PORT(com), (divisor >> 8) & 0x00FF);
-  outb(SERIAL_DATA_PORT(com), divisor & 0x00FF);
-}
-
-// Configure the line of the given serial port.
-// See https://en.wikipedia.org/wiki/Serial_port#Settings
-// We set the port to have a data length of 8 bits and some other stuff.
-static inline void serial_configure_line(const unsigned short com)
-{
-  outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
-}
-
-// Configure the buffer of the given serial port.
-// We enable FIFO, clear both the receiver and transmission FIFO queues
-// and set the queue size to 14 bytes.
-static inline void serial_configure_buffer(const unsigned short com)
-{
-  outb(SERIAL_FIFO_COMMAND_PORT(com), 0xC7);
-}
-
-// Configure the modem of the given serial port.
-// See https://en.wikipedia.org/wiki/Serial_port#Hardware_abstraction
-// We set RTS and DTR to be 1.
-static inline void serial_configure_modem(const unsigned short com)
-{
-  outb(SERIAL_MODEM_COMMAND_PORT(com), 0x03);
+  outb(serial_line_command_port(com), SERIAL_LINE_ENABLE_DLAB);
+  outb(com, (divisor >> 8) & 0x00FF);
+  outb(com, divisor & 0x00FF);
 }
 
 // Check whether the transmit FIFO queue is empty for a given com.
 // If bit 5 of the line status port is 1, the queue is empty.
 static inline int serial_is_transmit_fifo_empty(const unsigned int com)
 {
-  return inb(SERIAL_LINE_STATUS_PORT(com)) & 0x20;
+  return inb(serial_line_status_port(com)) & 0x20;
 }
 
 // Write a byte to a serial port.
@@ -71,7 +50,7 @@ static void serial_write_byte(
   )
 {
   while (!serial_is_transmit_fifo_empty(com)); // wait until it's empty
-  outb(SERIAL_DATA_PORT(com), byte);
+  outb(com, byte);
 }
 
 // Write a sequence of bytes of length `len` to a serial port.
@@ -87,7 +66,17 @@ void serial_write(
 void serial_init(const unsigned short com)
 {
   serial_configure_baud_rate(com, 3);
-  serial_configure_line(com);
-  serial_configure_modem(com);
-  serial_configure_buffer(com);
+
+  // Configure line -- set the port to have a data length of 8 bytes
+  // and some other stuff.
+  // See https://en.wikipedia.org/wiki/Serial_port#Settings
+  outb(serial_line_command_port(com), 0x03);
+
+  // Configure modem -- set RTS and DTR to be 1.
+  // See https://en.wikipedia.org/wiki/Serial_port#Hardware_abstraction
+  outb(serial_modem_command_port(com), 0x03);
+
+  // Configure buffer -- enable FIFO, clear both receiver and
+  // transmission FIFO queues, set queue size to 14 bytes.
+  outb(serial_fifo_command_port(com), 0xC7);
 }
