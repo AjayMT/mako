b81c3088c7c419c1f795ea85d80af90994c23af3
Ajay MT <ajay.tatachar@gmail.com>
2019-06-06 00:51:10 +0530

    util: strcmp

--- src/util/util.c
+++ src/util/util.c
@@ -6,6 +6,7 @@
 // Author: Ajay Tatachar <ajaymt2@illinois.edu>
 
 #include <stddef.h>
+#include <stdint.h>
 #include "util.h"
 
 void *u_memset(void *b, char c, size_t len)
@@ -26,4 +27,10 @@ size_t u_strlen(const char *s)
   size_t i = 0;
   for (; s[i] != '\0'; ++i);
   return i;
+}
+
+int32_t u_strcmp(const char *s1, const char *s2)
+{
+  for (; *s1 && *s1 == *s2; ++s1, ++s2);
+  return *s1 - *s2;
 }
--- src/util/util.h
+++ src/util/util.h
@@ -9,9 +9,11 @@
 #define _UTIL_H_
 
 #include <stddef.h>
+#include <stdint.h>
 
 void *u_memset(void *, char, size_t);
 void *u_memcpy(void *, const void *, size_t);
 size_t u_strlen(const char *);
+int32_t u_strcmp(const char *, const char *);
 
 #endif /* _UTIL_H_ */
