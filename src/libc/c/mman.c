44d2b37cd2b61479e6e69b11425e98410e40c18e
Ajay MT <ajay.tatachar@gmail.com>
2020-07-09 12:44:09 -0500

    libc: mmap stub

---
+++ src/libc/c/mman.c
@@ -0,0 +1,13 @@
+
+// mman.c
+//
+// mmap stub
+//
+// Author: Ajay Tatachar <ajaymt2@illinois.edu>
+
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t offset)
+{ return NULL; }
