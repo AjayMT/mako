f32af3d063b4c98991055c6310afce9d51625d74
Ajay MT <ajay.tatachar@gmail.com>
2021-05-18 00:15:17 -0500

    util: strncmp

--- src/util/util.c
+++ src/util/util.c
@@ -43,3 +43,11 @@ int32_t u_strcmp(const char *s1, const char *s2)
   for (; *s1 && *s1 == *s2; ++s1, ++s2);
   return *s1 - *s2;
 }
+
+int32_t u_strncmp(const char *s1, const char *s2, size_t n)
+{
+  uint32_t i = 0;
+  for (; *s1 && *s1 == *s2 && i < n; ++s1, ++s2, ++i);
+  if (i == n) return 0;
+  return *s1 - *s2;
+}
