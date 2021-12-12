262b89993640ad5339945cf2f0092fc2f2c7fbe5
Ajay MT <ajay.tatachar@gmail.com>
2021-12-12 12:23:24 -0600

    libnanoc: more syscalls

--- src/libnanoc/nanoc.c
+++ src/libnanoc/nanoc.c
@@ -7,6 +7,7 @@
 
 #include <syscall/syscall_nums.h>
 #include <stdint.h>
+#include <stddef.h>
 
 int32_t _syscall0(const uint32_t num)
 {
@@ -73,8 +74,49 @@ int32_t _syscall4(
 uint32_t write(uint32_t fd, const void *buf, uint32_t count)
 { return _syscall3(SYSCALL_WRITE, fd, (uint32_t)buf, count); }
 
+uint32_t read(uint32_t fd, const void *buf, uint32_t count)
+{ return _syscall3(SYSCALL_READ, fd, (uint32_t)buf, count); }
+
+int32_t open(char *path, int32_t flags, uint32_t mode)
+{ return _syscall3(SYSCALL_OPEN, (uint32_t)path, (uint32_t)flags, mode); }
+
+int32_t close(uint32_t fd)
+{ return _syscall1(SYSCALL_CLOSE, fd); }
+
+uint32_t fork()
+{ return _syscall0(SYSCALL_FORK); }
+
+int32_t execve(char *path, char **argv, char **envp)
+{
+  char *largv[] = { NULL };
+  char *lenvp[] = { NULL };
+  if (argv == NULL) argv = largv;
+  if (envp == NULL) envp = largv;
+  return _syscall3(
+    SYSCALL_EXECVE, (uint32_t)path, (uint32_t)argv, (uint32_t)envp
+    );
+}
+
 void exit(int32_t status)
 { _syscall1(SYSCALL_EXIT, (uint32_t)status); }
+
+uint32_t pagealloc(uint32_t npages)
+{ return _syscall1(SYSCALL_PAGEALLOC, npages); }
+
+int32_t pagefree(uint32_t vaddr, uint32_t npages)
+{ return _syscall2(SYSCALL_PAGEFREE, vaddr, npages); }
+
+#define PAGE_SIZE 0x1000
+
+void *malloc(size_t size)
+{
+  if (size != (size & 0xFFFFF000))
+    size = (size & 0xFFFFF000) + PAGE_SIZE;
+  return (void *)pagealloc(size >> 12);
+}
+
+void free(void *ptr)
+{ return; }
 
 #define ARGV_ADDR 0xBFFFF000
 
