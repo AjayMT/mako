b6c5d680a529675bd57fd8e6f5f9f825e47fe975
Ajay Tatachar <ajay_m_tatachar@apple.com>
2024-12-11 13:35:19 -0500

    read: correctly read and write entire files

--- src/bin/read.c
+++ src/bin/read.c
@@ -12,21 +12,36 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc <= 1)
+  if (argc <= 1) {
+    printf("Usage: read <filename>\n");
     return 1;
+  }
 
   char *path = argv[1];
   struct stat buf;
-  int32_t res = stat(path, &buf);
-  if (res)
+  int32_t err = stat(path, &buf);
+  if (err)
     return 1;
 
-  char *text = malloc(buf.st_size + 1);
-  FILE *f = fopen(path, "r");
-  fread(text, buf.st_size, 1, f);
-  text[buf.st_size] = 0;
+  char *text = malloc(buf.st_size);
+  if (text == NULL)
+    return 1;
 
-  fwrite(text, buf.st_size, 1, stdout);
+  FILE *f = fopen(path, "r");
+  if (f == NULL)
+    return 1;
+
+  size_t nread = fread(text, 1, buf.st_size, f);
+  size_t nwritten = fwrite(text, 1, nread, stdout);
+  while (nwritten < nread)
+    nwritten += fwrite(text + nwritten, 1, nread - nwritten, stdout);
+
+  while (nread < (size_t)buf.st_size) {
+    nread += fread(text, 1, buf.st_size - nread, f);
+    nwritten = fwrite(text, 1, nread, f);
+    while (nwritten < nread)
+      nwritten += fwrite(text + nwritten, 1, nread - nwritten, stdout);
+  }
 
   return 0;
 }
